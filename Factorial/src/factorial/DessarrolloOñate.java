33b74c630ad45a1ac41fb5d1cc430a3eebbf1311
Rolando <Rolando@172.21.106.234>
2015-11-10 09:06:54 -0500

    prueba

---
+++ Factorial/src/factorial/DessarrolloOñate.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package factorial;
+
+/**
+ *
+ * @author Rolando
+ */
+public class DessarrolloOñate {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // como estan
+    }
+    
+}
